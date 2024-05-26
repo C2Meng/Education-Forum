c4fba98f1e32aa2330d4fdae2d7fb6a75b7585f5
Chan Chuan Meng <chancm530@gmail.com>
2024-05-26 21:43:57 +0800

    added html form for creating quiz, added database for quiz, and quiz form

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hello kitty'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db' #this made the whole thing work
+    db.init_app(app) # init connection to db
 
     from .views import views
     from .auth import auth
     from .question import question
+
+
+    
+   
+
+    from .models import Quiz, Question, Answer
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(question, url_prefix='/' )
     
     return app 
+
