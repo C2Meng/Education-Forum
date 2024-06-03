695c95dce1c50d126422bf3d0120c5ba2ca6ef09
winnielee0401 <winnielee0401@gmail.com>
2024-06-04 00:20:12 +0800

    Updating working tree

--- website/teacher.py
+++ website/teacher.py
@@ -11,8 +11,16 @@ def discovery():
 
 @teacher.route('/admin_home')
 def admin():
+<<<<<<< HEAD
     return render_template('admin_home.html', user=current_user)
 
 @question.route('/quiz')
 def display_quiz():
     return render_template('quiz.html', user=current_user)
+=======
+    return render_template('admin_home.html')
+
+@teacher.route('/quiz_data')
+def quiz_data():
+    return render_template('quiz_data.html')
+>>>>>>> main
