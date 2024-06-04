e3aa6791ee4f360f13ed2d8951c5bd703bb87618
winnielee0401 <winnielee0401@gmail.com>
2024-06-04 09:27:47 +0800

    Adjusted position of logout button

--- website/teacher.py
+++ website/teacher.py
@@ -3,7 +3,6 @@ from flask_login import current_user
 
 teacher = Blueprint('teacher', __name__)
 
-
 @teacher.route('/discovery')
 def discovery():
     return render_template('discovery.html', user=current_user)
