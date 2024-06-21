2d2ce4282a8712fff483f8772b7ea0f3a319d191
winnielee0401 <winnielee0401@gmail.com>
2024-06-21 19:13:23 +0800

    Merging main into winnie

--- website/auth.py
+++ website/auth.py
@@ -9,20 +9,27 @@ from sqlalchemy.exc import IntegrityError
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/') #lead to login page after running the app
+def index():
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        user_status = request.form.get('user_status') 
+        
 
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                if user.user_status ==  'Teacher':
+                    return redirect(url_for('teacher.admin_home'))
+                else:
+                    return redirect(url_for('question.my_quiz'))
             else:
                 flash('Incorrect password. Please try again', category='error')
         else:
@@ -36,7 +43,7 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -70,17 +77,21 @@ def sign_up():
                     db.session.commit()
                     login_user(new_user, remember=True)
                     flash('Account created!', category='success')
-                    return redirect(url_for('views.home'))
+                    if new_user.user_status ==  'Teacher':
+                        return redirect(url_for('teacher.admin_home'))
+                    else:
+                        return redirect(url_for('question.my_quiz'))
                 except IntegrityError:
                     db.session.rollback()
                   
 
-    return render_template("sign_up.html", user=current_user)
-
+    
+    #this remembers user's input if error is found in form
     full_name = request.form.get('fullName','')
-    password1 = request.form.get('password1','') #redundant codes to store the info user keyed in, values keyed in html form
+    password1 = request.form.get('password1','') #redundant codes to store the info user keyed in, values called in html form
     email = request.form.get('email', '')
     password2 = request.form.get('password2','')
     user_status = request.form.get('user_status')
+
     return render_template("sign_up.html", user=current_user, full_name=full_name, 
-                           password1=password1, email=email, password2=password2, user_status=user_status)
+                            password1=password1, email=email, password2=password2, user_status=user_status)
